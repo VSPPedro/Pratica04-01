762d1db160dd06eb5e2bf20e720f7e8a6e22b34e
Pedro Paiva <pedrovsp109@gmail.com>
2017-05-15 11:19:53 -0300

    3)Modifique o servlet ValidaLoginServlet

--- src/br/edu/ifpb/pweb/ValidaLoginServlet.java
+++ src/br/edu/ifpb/pweb/ValidaLoginServlet.java
@@ -2,12 +2,14 @@ package br.edu.ifpb.pweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class ValidaLoginServlet
@@ -28,16 +30,34 @@ public class ValidaLoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Properties passwdFile;
+		passwdFile = new Properties();
+		try{
+			passwdFile.load(this.getServletContext().getResourceAsStream("/WEB-INF/senhas.properties"));
+		} catch(IOException e) {
+			response.sendError( response.SC_BAD_REQUEST, "Erro ao ler arquivo de senhas!");
+			return;  //cai fora do servlet
+		}
+		
 		// pega os parâmetros fornecidos pelo formulário
 		String login = request.getParameter("login");
 		String senha= request.getParameter("senha");
 		
+		// verificaseo usuário é valido
+		if (passwdFile.containsKey(login) && senha.equals(passwdFile.get(login))) {
+			request.setAttribute("login", login);
+			request.getRequestDispatcher("loginValido.jsp").forward(request, response);
+		} else{
+			response.sendRedirect("index.html");
+		}
+		
+		/*
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>");
 		out.println("<h1>Login:"+login+"</h1>");
 		out.println("<h1>Senha:"+senha+"</h1>");
-		out.println("</html></body>");
+		out.println("</html></body>");*/
 	}
 
 	/**
